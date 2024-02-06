cd33ecb9d8553daeeb7595bd411fd971fbf5700e
ygmrbrndrhm <yagmurbernaderhem@gmail.com>
2024-02-06 20:12:25 +0300

    fixed

---
+++ Assets/BrightnessManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessManager : MonoBehaviour
+{
+    [SerializeField] private OVRPassthroughLayer passthroughLayer;
+    
+    private void Start()
+    {
+        AdjustBrightness(0f);
+    }
+    public void AdjustBrightness(float brightnessAmount)
+    {
+        if (passthroughLayer != null)
+        {
+
+            passthroughLayer.SetBrightnessContrastSaturation(brightnessAmount);
+
+        }
+    }
+    public void AdjustBrightnessGradually()
+    {
+        if (passthroughLayer != null)
+        {
+            float currentBrightness = passthroughLayer.colorMapEditorBrightness;
+           
+
+        }
+        
+
+    }
+}
