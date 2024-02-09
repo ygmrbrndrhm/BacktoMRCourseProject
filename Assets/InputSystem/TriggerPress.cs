ceb056acc61340a9f4b062737ec65a3649ba2bfc
ygmrbrndrhm <yagmurbernaderhem@gmail.com>
2024-02-09 11:35:47 +0300

    HapticFeedback

--- Assets/InputSystem/TriggerPress.cs
+++ Assets/InputSystem/TriggerPress.cs
@@ -1,4 +1,5 @@
 
+using Oculus.Haptics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +10,13 @@ public class TriggerPress : MonoBehaviour
     private Input input = null;
     private float triggerpress;
     [SerializeField] public GameObject obj;
+   // [SerializeField] private HapticClip hapticClip;
+    //private HapticClipPlayer hapticClipPlayer;
+    [SerializeField] private float hapticDuration = 0.1f;
     private void Awake()
     {
         input = new Input();
+        //hapticClipPlayer = new HapticClipPlayer(hapticClip);
     }
     private void OnEnable()
     {
@@ -27,6 +32,9 @@ public class TriggerPress : MonoBehaviour
     }
     private void OnTriggerPerformed(InputAction.CallbackContext value)
     {
+       // PlayHapticFeedback();
+        VibrateTriggerBasedOnPressure(triggerpress);
+        //StartCoroutine(TriggerHapticFeedback());//1
         triggerpress = value.ReadValue<float>();
         ColorChange();
     }
@@ -48,5 +56,29 @@ public class TriggerPress : MonoBehaviour
              obj.GetComponent<Renderer>().material.color = Color.white;
         }
     }
+
+
+    private void VibrateTriggerBasedOnPressure(float triggerpress)
+    {
+        OVRInput.SetControllerLocalizedVibration(OVRInput.HapticsLocation.Hand, triggerpress,1, OVRInput.Controller.Active);
+    }
+    //private IEnumerator TriggerHapticFeedback()//1
+    //{
+    //    OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+    //    yield return new WaitForSeconds(hapticDuration);
+    //    OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
+    //}//1
+    //private void PlayHapticFeedback()
+    //{
+    //    hapticClipPlayer.Play(Controller.Right);
+    //}
+    //private void OnDestroy()
+    //{
+    //    hapticClipPlayer.Dispose();
+    //}
+    //private void OnApplicationQuit()
+    //{
+    //    Haptics.Instance.Dispose();
+    //}
 }
 
